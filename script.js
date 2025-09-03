3b36db83c7fa6057e5449f28a5222e2a58ca0408
moxlyy <soapyjoechannel@gmail.com>
2025-09-03 22:57:08 +0000

    prototype

---
+++ script.js
@@ -0,0 +1,72 @@
+const canvas = document.getElementById("screen");
+const context = canvas.getContext("2d");
+const imageData = context.getImageData(0, 0, 256, 240);
+
+const nes = new jsnes.NES({
+  onFrame(frameBuffer) {
+    for (let i = 0; i < frameBuffer.length; i++) {
+      imageData.data[i * 4 + 0] = (frameBuffer[i] >> 16) & 0xff;
+      imageData.data[i * 4 + 1] = (frameBuffer[i] >> 8) & 0xff;
+      imageData.data[i * 4 + 2] = frameBuffer[i] & 0xff;
+      imageData.data[i * 4 + 3] = 0xff;
+    }
+    context.putImageData(imageData, 0, 0);
+  },
+  onStatusUpdate: console.log,
+  onAudioSample: () => {}
+});
+
+// load rom file
+document.getElementById("romLoader").addEventListener("change", e => {
+  const file = e.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function() {
+      nes.loadROM(reader.result);
+      frameLoop();
+    };
+    reader.readAsBinaryString(file);
+  }
+});
+
+// render loop
+function frameLoop() {
+  nes.frame();
+  requestAnimationFrame(frameLoop);
+}
+
+// fullscreen support
+function goFullscreen() {
+  if (canvas.requestFullscreen) {
+    canvas.requestFullscreen();
+  } else if (canvas.webkitRequestFullscreen) {
+    canvas.webkitRequestFullscreen();
+  }
+}
+
+// 🎮 keyboard input mapping
+document.addEventListener("keydown", e => {
+  switch (e.key.toLowerCase()) {
+    case "arrowup": nes.buttonDown(1, jsnes.Controller.BUTTON_UP); break;
+    case "arrowdown": nes.buttonDown(1, jsnes.Controller.BUTTON_DOWN); break;
+    case "arrowleft": nes.buttonDown(1, jsnes.Controller.BUTTON_LEFT); break;
+    case "arrowright": nes.buttonDown(1, jsnes.Controller.BUTTON_RIGHT); break;
+    case "z": nes.buttonDown(1, jsnes.Controller.BUTTON_A); break; // A
+    case "x": nes.buttonDown(1, jsnes.Controller.BUTTON_B); break; // B
+    case "enter": nes.buttonDown(1, jsnes.Controller.BUTTON_START); break;
+    case "shift": nes.buttonDown(1, jsnes.Controller.BUTTON_SELECT); break;
+  }
+});
+
+document.addEventListener("keyup", e => {
+  switch (e.key.toLowerCase()) {
+    case "arrowup": nes.buttonUp(1, jsnes.Controller.BUTTON_UP); break;
+    case "arrowdown": nes.buttonUp(1, jsnes.Controller.BUTTON_DOWN); break;
+    case "arrowleft": nes.buttonUp(1, jsnes.Controller.BUTTON_LEFT); break;
+    case "arrowright": nes.buttonUp(1, jsnes.Controller.BUTTON_RIGHT); break;
+    case "z": nes.buttonUp(1, jsnes.Controller.BUTTON_A); break;
+    case "x": nes.buttonUp(1, jsnes.Controller.BUTTON_B); break;
+    case "enter": nes.buttonUp(1, jsnes.Controller.BUTTON_START); break;
+    case "shift": nes.buttonUp(1, jsnes.Controller.BUTTON_SELECT); break;
+  }
+});
